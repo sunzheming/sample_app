16062f194523d2b672c2359b1a9f7dedbd218d9f
Jimmy <jimmysun123@gmail.com>
2016-06-04 16:13:37 +0000

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+def gravatar_for(user)
+  gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+  gracatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+  image_tag(gracatar_url, alt:user.name, class: "gravatar")
+end
+
+
 end
