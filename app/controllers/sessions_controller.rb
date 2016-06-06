32e263132b978ccab2ea3e4bb053fb7ddda41678
Jimmy <jimmysun123@gmail.com>
2016-06-06 00:11:13 +0000

    Finish log in/log out

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      # if params[:session][:remember_me] == '1'
+      #   remember(user)
+      # else
+      #   forget(user)
+      # end
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      
+      redirect_to user
+    else
+      render 'new'
+      flash.now[:danger] = "Invalid email/password combination"
+    end
+  end
+  
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+    
+end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -25,6 +25,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                           password_confirmation: password}
     end
     assert_template 'users/show'
+    assert is_logged_in?
+    
   end
   
 end
