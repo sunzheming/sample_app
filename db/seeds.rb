a9b7c7c8e962fed3d267b2609a5360990c5da6c0
Jimmy <jimmysun123@gmail.com>
2016-06-07 17:31:48 +0000

    Add acount activations

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,9 @@ User.create!(name: "Jimmy Sun",
             email: "jimmysun123@gmail.com",
             password: "sd2129865",
             password_confirmation: "sd2129865",
-            admin: true)
+            admin: true,
+            activated: true,
+            activated_at: Time.zone.now)
 
 
 99.times do |n|
@@ -19,5 +21,7 @@ User.create!(name: "Jimmy Sun",
   User.create!(name: name,
                 email: email,
                 password: password,
-                password_confirmation: password)
+                password_confirmation: password,
+                activated: true,
+                activated_at: Time.zone.now)
 end
