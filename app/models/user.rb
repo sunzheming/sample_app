91473eda690bf3784ba11af6a99c41dc2f52ef1c
Jimmy <jimmysun123@gmail.com>
2016-06-12 16:44:25 +0000

    2 resume

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
  
   before_save :downcase_email
   before_create :create_activation_digest
+  #用户删除微博也删除
+  has_many :microposts, dependent: :destroy
   
   validates :name, presence: true, length: { maximum: 50 } 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
